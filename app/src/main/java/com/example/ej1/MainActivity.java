6da291c3e0a35b0536711d7817488ac7d6db4001
esTechVMG <vicente.maroto@escuelaestech.es>
2020-01-28 04:49:38 +0100

    Dark Mode 90% implemented

--- app/src/main/java/com/example/ej1/MainActivity.java
+++ app/src/main/java/com/example/ej1/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.ej1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button plus;
     private  Button minus;
     private TextView output1;
     private EditText modNumber;
+    private ToggleButton theme;
+    private int darkColor,lightColor,lightTint,darkTint;
+    private LinearLayout[] layouts=new LinearLayout[2];
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //INIT
+
+        resultText=findViewById(R.id.result);
+        layouts[0]=findViewById(R.id.firstContainer);
+        layouts[1]=findViewById(R.id.themeContainer);
+        darkColor=getResources().getColor(R.color.darkTheme);
+        lightColor=getResources().getColor(R.color.whiteTheme);
+        lightTint=getResources().getColor(R.color.tint_light);
+        darkTint=getResources().getColor(R.color.tint_dark);
+        theme=findViewById(R.id.themeChoose);
         minus = findViewById(R.id.suma);
         plus = findViewById(R.id.resta);
         output1 = findViewById(R.id.outputnumber1);
@@ -27,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         minus.setOnClickListener(this);
         plus.setOnClickListener(this);
         modNumber.setOnClickListener(this);
+        theme.setOnClickListener(this);
+        getResources().getColor(R.color.whiteTheme);
 
     }
     private int actualNum=0;
@@ -43,7 +59,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         output1.setText(String.valueOf(actualNum));
     }
+    public void updateTheme(){
+        if(theme.isChecked()){
+            setTheme(lightColor,darkColor,darkTint,lightTint,R.drawable.round_corner_bg_dark);//DARK THEME
+        }else{
+            setTheme(darkColor,lightColor,lightTint,darkTint,R.drawable.round_corner_bg_light);//LIGHT THEME
+        }
 
+    }
+    public void setTheme(int textColor,int backColor,int tintColor,int hintColor,int drawBG){
+        colorButtonUpdate(plus,textColor,backColor);
+        colorButtonUpdate(minus,textColor,backColor);
+        resultText.setTextColor(textColor);
+        modNumber.setTextColor(textColor);
+        modNumber.setHintTextColor(hintColor);
+        layouts[1].setBackgroundColor(backColor);
+        theme.setTextColor(textColor);
+        int pL = layouts[0].getPaddingLeft();
+        int pT = layouts[0].getPaddingTop();
+        int pR = layouts[0].getPaddingRight();
+        int pB = layouts[0].getPaddingBottom();
+        layouts[0].setBackgroundResource(drawBG);
+        layouts[0].setPadding(pL,pT,pR,pB);
+    }
+    public void colorButtonUpdate(Button b,int textColor,int backColor){
+        b.setTextColor(textColor);
+        b.getBackground().setTint(backColor);
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -56,6 +98,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.modnum:
                 modNumber.requestFocus();
                 break;
+            case R.id.themeChoose:
+                updateTheme();
         }
     }
 }
